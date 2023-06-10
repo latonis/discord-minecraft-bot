709fecfcb4104cf61524d56b30a83c47d2fef517
Jacob Latonis <latonis@wisc.edu>
2023-06-10 18:13:08 -0500

    server check function implemented

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,13 +13,77 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+type MCStatus struct {
+	Online      bool   `json:"online"`
+	Host        string `json:"host"`
+	Port        int    `json:"port"`
+	EulaBlocked bool   `json:"eula_blocked"`
+	RetrievedAt int64  `json:"retrieved_at"`
+	ExpiresAt   int64  `json:"expires_at"`
+	Version     struct {
+		NameRaw   string `json:"name_raw"`
+		NameClean string `json:"name_clean"`
+		NameHTML  string `json:"name_html"`
+		Protocol  int    `json:"protocol"`
+	} `json:"version"`
+	Players struct {
+		Online int `json:"online"`
+		Max    int `json:"max"`
+		List   []struct {
+			UUID      string `json:"uuid"`
+			NameRaw   string `json:"name_raw"`
+			NameClean string `json:"name_clean"`
+			NameHTML  string `json:"name_html"`
+		} `json:"list"`
+	} `json:"players"`
+	Motd struct {
+		Raw   string `json:"raw"`
+		Clean string `json:"clean"`
+		HTML  string `json:"html"`
+	} `json:"motd"`
+	Icon     string `json:"icon"`
+	Mods     []any  `json:"mods"`
+	Software any    `json:"software"`
+	Plugins  []any  `json:"plugins"`
+}
+
+func check_err(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func server_status(server string, bedrock bool) {
+	fmt.Println("Getting information for server: " + server)
+	platform := "java"
+
+	if bedrock {
+		platform = "bedrock"
+	}
+
+	resp, err := http.Get("https://api.mcstatus.io/v2/status/" + platform + "/" + server)
+	check_err(err)
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	check_err(err)
+
+	var result MCStatus
+	err = json.Unmarshal(body, &result)
+	check_err(err)
+
+	str, err := json.MarshalIndent(result, "", "\t")
+	check_err(err)
+	
+	fmt.Println(string(str))
+
+}
+
 func main() {
 	discord_key := os.Getenv("DISCORD_KEY")
 	session, err := discordgo.New("Bot " + discord_key)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	check_err(err)
 
 	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if m.Author.ID == s.State.User.ID {
@@ -33,14 +100,12 @@ func main() {
 
 	err = session.Open()
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	check_err(err)
 
 	defer session.Close()
 
 	fmt.Println("Discord bot spinning up")
-
+	server_status(os.Getenv("MINECRAFT_SERVER"), false)
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
