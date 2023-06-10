4d3737f3608e76574a15e6b154b5b0c04f2f0878
Jacob Latonis <latonis@wisc.edu>
2023-06-10 17:14:38 -0500

    hello world message donezo

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module discord-minecraft-bot
 
 go 1.20
+
+require github.com/bwmarrin/discordgo v0.27.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func main() {
-	fmt.Println("Hello there, Discord bot")
+	discord_key := os.Getenv("DISCORD_KEY")
+	session, err := discordgo.New("Bot " + discord_key)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if m.Content == "hello" {
+			s.ChannelMessageSend(m.ChannelID, "world")
+		}
+
+	})
+
+	session.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = session.Open()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer session.Close()
+
+	fmt.Println("Discord bot spinning up")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
 }
